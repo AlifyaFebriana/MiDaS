a9b714c4ecc3d73d729fe649061ce01039272de2
AlifyaFebriana <alifya.f@mhs.unsyiah.ac.id>
2023-05-10 13:34:22 +0800

    visualize the 3d point cloud

---
+++ read_ply.py
@@ -0,0 +1,11 @@
+import open3d as o3d
+
+def visualize_point_cloud(ply_file):
+    # load ply file
+    point_cloud = o3d.io.read_point_cloud(ply_file)
+    print(point_cloud)
+
+    # visualize
+    o3d.visualization.draw_geometries([point_cloud])
+
+visualize_point_cloud("/home/junaid/alifya/Bean/3DBEAN/point-cloud/premium (2071).ply")
